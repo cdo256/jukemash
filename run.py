1c8665eb15560f93aeabac27bfbb37f9f10ed964
Alice Gowler <psyag8@exmail.nottingham.ac.uk>
2024-11-16 15:37:12 +0000

    add dummy api

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +8,9 @@ def home():
 @app.route('/static/<path:filename>')
 def static_files(filename):
     return send_from_directory('static', filename)
+
+@app.route("/api/create_game", methods=['POST'])
+def create_game():
+    data = request.get_json()
+
+    return jsonify({'message': "Yippee !!!!!"}), 201
