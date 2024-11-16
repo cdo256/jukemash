03b804b1691ca8603d1cc656477ce9675d7dd12e
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2024-11-16 16:01:23 +0000

    Proxy /api requests on vite dev server to flask server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/static",
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000/",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
