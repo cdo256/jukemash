c69f6e338488027c3126e3f857b2ee324623075e
Alice Gowler <psyag8@exmail.nottingham.ac.uk>
2024-11-16 15:22:16 +0000

    Refactor project structure

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-from src.app import create_app
+import utils
+from flask import Flask, send_from_directory
+app = Flask(__name__)
 
-app = create_app()
+@app.route('/')
+def home():
+    return send_from_directory('static', "index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
+
