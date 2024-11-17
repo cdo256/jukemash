07ef42d9579b2b4c9d2625cd65d88adc11143425
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2024-11-17 08:35:32 +0000

    Fix the connecting UI

--- client/src/player/PlayerPage.tsx
+++ client/src/player/PlayerPage.tsx
@@ -1,66 +1,74 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-type GameState = "UNCONNECTED" | "CONNECTING" | "WAITING" | "GAME_START" | "ROUND_START" | "SONG_PLAYING" | "PLAYER_BUZZED" | "OTHER_BUZZED" | "ROUND_END" | "GAME_END";
+type GameState =
+  | "UNCONNECTED"
+  | "CONNECTING"
+  | "WAITING"
+  //| "GAME_START"
+  | "ROUND_START"
+  | "SONG_PLAYING"
+  | "PLAYER_BUZZED"
+  | "OTHER_BUZZED"
+  | "ROUND_END";
+//| "GAME_END";
 
-interface ConnectProps {
-  name: string;
-  code: string;
-  setState: (state: GameState) => void;
-}
-
-function connect({ name, code, setState }: ConnectProps) {
-  console.log(`connect ${name} ${code}`)
+export function PlayerPage({ onBack }: { onBack: () => void }) {
+  const [state, setState] = useState<GameState>("UNCONNECTED");
+  const [name, setName] = useState<string>("");
+  const [code, setCode] = useState<string>("");
   const connectMutation = useMutation({
     mutationFn: async () => {
+      console.log(`connect ${name} ${code}`);
       return await axios.post("/api/join_game", {
         name: name,
-        gameCode: code
-      })
+        gameCode: code,
+      });
     },
     onSuccess: () => {
       setState("WAITING");
     },
     onError: () => {
       setState("UNCONNECTED");
-    }
+    },
   });
-  useEffect(() => {
-    connectMutation.mutate();
-  }, []);
-  
-  //if (connectMutation.isPending) {
-  //  return <p>Loading...</p>;
-  //} else if (!connectMutation.isSuccess) {
-  //  return <p>Error</p>;
-  //}
-
-  //return (
-  //  <>
-  //    <h1>JukeMash!!!</h1>
-  //    <h2>Waiting for other players...</h2>
-  //  </>
-  //)
-}
-
-export function PlayerPage({ onBack }: { onBack: () => void }) {
-  const [state, setState] = useState<GameState>("UNCONNECTED");
-  const [name, setName] = useState<string>('');
-  const [code, setCode] = useState<string>('');
 
   if (state == "UNCONNECTED") {
-    return (<>
-      <button className='back' onClick={onBack}></button>
-      <label>Nickname:</label>
-      <input type='text' value={name} onChange={(e) => setName(e.target.value)} /> 
-      <label>4-letter connect code:</label>
-      <input type='text' value={code} onChange={(e) => setCode(e.target.value)} /> 
-      <button onClick={() => connect({name, code, setState})}>Connect</button>
-    </>);
+    return (
+      <>
+        <button className="back" onClick={onBack}></button>
+        <label>Nickname:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>4-letter connect code:</label>
+        <input
+          type="text"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <button
+          disabled={connectMutation.isPending}
+          onClick={() => connectMutation.mutate()}
+        >
+          Connect
+        </button>
+      </>
+    );
   } else if (state == "WAITING") {
-    return (<><h2>Waiting for other players...</h2></>);
+    return (
+      <>
+        <h2>Waiting for other players...</h2>
+      </>
+    );
   } else {
-    return (<><h2>Unknown state</h2></>);
+    return (
+      <>
+        <h2>Unknown state</h2>
+      </>
+    );
   }
 }
