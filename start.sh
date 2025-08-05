058f314878b33c5986e59f4695bc0ccf1ca25634
Alice Gowler <alicegowler@outlook.com>
2025-08-06 00:40:07 +0100

    Update to use gunicorn WSGI server

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 poetry config --local installer.no-binary ":all:"
 poetry install
-poetry run python run.py
+
+if [ "$USE_SSL" = "true" ]; then
+  poetry run gunicorn --bind 0.0.0.0:443 \
+    --keyfile /ssl/privkey.pem \
+    --certfile /ssl/fullchain.pem \
+    run:app
+else
+  poetry run gunicorn --bind 0.0.0.0:80 \
+    run:app
+fi
